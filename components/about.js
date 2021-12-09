4e35d91bbbd658697fddccf76ad70baf247565bb
vennylee <hyungsup42134@gmail.com>
2021-12-09 15:38:10 +0900

    design(section): Organize code

--- components/about.js
+++ components/about.js
@@ -51,10 +51,10 @@ window.addEventListener("scroll", parallaxFunc);
 
 function parallaxFunc() {
   const scrollY = window.scrollY + aboutTop;
-  leafBlue.style.right = `${scrollY * 0.45}px`;
+  leafBlue.style.right = `${scrollY * 0.65}px`;
   leafGreen.style.top = `${scrollY * 0.1}px`;
-  leafGreen.style.left = `${scrollY * 0.4}px`;
+  leafGreen.style.left = `${scrollY * 0.5}px`;
   leafRedBig.style.top = `${scrollY * 0.15}px`;
-  leafRedBig.style.right = `${scrollY * 0.3}px`;
-  leafRedSmall.style.left = `${scrollY * 0.5}px`;
+  leafRedBig.style.right = `${scrollY * 0.6}px`;
+  leafRedSmall.style.left = `${scrollY * 0.6}px`;
 }
